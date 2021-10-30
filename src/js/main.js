0391daae6c91882ee240bb366dd67edccd5a09a1
SheikhRashed <devrashed06@gmail.com>
2021-10-30 16:30:07 +0600

    instructor card added height and removed border radius and slider autoplay activate

--- src/js/main.js
+++ src/js/main.js
@@ -480,7 +480,7 @@ function menuClick(current) {
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
     prevArrow:
       ' <span class="slick-arrow--left"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg></span>',
